5e38bbe072091a1471329ed851be550743f0384c
anjalisangwan95 <anjali_a_sangwan@hotmail.com>
2020-05-27 22:24:19 +0200

    updated omos page

--- page-om-os.php
+++ page-om-os.php
@@ -1,21 +1,19 @@
 <?php get_header(); ?>
 
 <body > 
-<!-- Hovedtitel med beskrivelse -->
-  <main role="main" class="flex-shrink-0">
+<!-- Main titel and a descrition of Dahlgaard -->
+<main role="main" class="flex-shrink-0">
     <div class="container">
-      <h1 class=" display-3 font-weight-bold text-white">
+      <h1 class=" display-3 font-weight- text-white">
         Vi leverer individuelle kvalitetsløsninger. </h1>    
     </div>
-
   </main>
 
- 
-<!-- Vores vision-->
+<!-- Section nr 1 Vores vision-->
   <div class="container">
     <div class="row row-cols-2 mt-7 pt-7">
-      <div class="col"><h2 class="  text-white font-weight-bold">
-        Vores vision </h2></div>
+      <div class="col"><h3 class="  text-white font-weight-bold">
+        Vores vision </h3></div>
       <div class="col text-white">Vi vil være førende inden for renovering af fredet ejendomme. Vi har
         allerede en dybdegående viden om, sammensætningen af historiske og
         fredet ejendomme i det offentlige og private rum. Vi vil bestræbe os på
@@ -24,113 +22,106 @@
         historiske ejendomme.</p></div>
       <div class="col"></div>
       <div class="col"><p></div>
-    </div>
+      </div>
   </div>
-
-
  
-<!-- Vores Værdier -->
-<div class="container-values">
-  <div class="d-flex flex-row bd-highlight ">
-    <div class="p-2 bd-highlight">
-      <h3 class=" display-3 text-danger font-weight-bold pl-4 pt-5">
-        Vores<br>værdier </h3>
-      <p>Vores værdier handler grundlæggende 
+<!-- Start af Section nr 2 vores værdier -->
+<div class="container-fluid">
+  <div class="wrapper">
+  <div class="row row-cols-2 mt-7 pt-7 text-primary "style="background-color: white;">
+    <div class="col pl-7">
+      <h1 class="font-weight-bold text-danger">
+      Vores <br>værdier</h1> <p>Vores værdier handler grundlæggende 
         om at alle medarbejder yder deres bedste, 
         og med fokus  på kvalitet og ordentlighed 
          skal vi altid mødes med en positiv energi.</p>
     </div>
 
-    <div class="p-2 bd-highlight">
-
-      <div class="tab-content">
-        <div id="punkt1" class="tab-pane fade show active ">
-          <h3>Kvalitetshåndværk</h3>
+<!-- Navtab slideshow af værdier -->
+    <div class="col">
+      <div class="tab-content p-7 text-primary">
+        <div id="punkt1" class="tab-pane fade show active pt-4">
+          <h4 class="font-weight-bold">Kvalitetshåndværk</h4>
           <p>Vi går aldrig på kompromis med kvaliteten – det er her vi differentierer os! Vores håndværk lever altid
             op til den gældende lovgivning – og lidt til!
-
           </p>
         </div>
-        <div id="punkt2" class="tab-pane fade ">
-            <h3>Fleksibilitet</h3>
+        <div id="punkt2" class="tab-pane fade">
+            <h4 class="font-weight-bold">Fleksibilitet</h4>
           <p>Vi er stolte af den store fleksibilitet, vi tilbyder vores kunder. Vores løsninger er skræddersyet til
             behovet.</p>
         </div>
-        <div id="punkt3" class="tab-pane fade ">
-          <h3>Engagemenet</h3>
+        <div id="punkt3" class="tab-pane fade">
+          <h4 class="font-weight-bold">Engagemenet</h4>
           <p>Vi forventer et højt engagement af os selv. Kun sådan kan vi skabe de bedste relationer, de bedste
             løsninger og det bedste arbejdsklima! Vi deltager aktivt – og er frem for alt engagerede.</p>
         </div>
-        <div id="punkt4" class="tab-pane fade ">
-          <h3>Nytænkning</h3>
+        <div id="punkt4" class="tab-pane fade">
+          <h4 class="font-weight-bold">Nytænkning</h4>
           <p>Vi har altid fokus på innovative tiltag. For at kunne levere den bedst mulige kvalitet og service,
             derfor satser vi på nytænkning og investerer i nyeste teknologi. Både for optimering men også af
             miljøhensyn.</p>
         </div>
-        <div id="punkt5" class="tab-pane fade ">
-          <h3>Den gode relation.
-
-          </h3>
+        <div id="punkt5" class="tab-pane fade">
+          <h4 class="font-weight-bold">Den gode relation.
+          </h4>
           <p>Vi behandler andre, som vi selv ønsker at blive behandlet. Det tager år at opbygge en stærk relation,
             men kun få sekunder at miste den. Derfor er det en vigtig del af Dahlgaard kulturen, at du som kunde
             bliver mødt af glade, positive og hjælpsomme medarbejder.</p>
         </div>
-        <div id="punkt6" class="tab-pane fade ">
-          <h3>CSR (Corporate Social Responsibility)
-
-          </h3>
+        <div id="punkt6" class="tab-pane fade">
+          <h4 class="font-weight-bold">CSR (Corporate Social Responsibility)
+          </h4>
           <p>Dahlgaard Entreprise ApS vil biddrage i samfundet, som en anstændig virksomhed, der tager del i at
             uddanne nye fagfolk. Pt. har vi 6 lærlinge.
-
           </p>
         </div>
-
       </div>
-</div>
+    </div>
+    <div class="col">
+  </div>
 
+<!-- Nav tabs buttons/knapper -->
+    <div class="col pl-7 pb-5">
       <ul class="nav nav-tabs">
-        <li class="show active"><a data-toggle="tab" href="#punkt1"> 1</a></li>
+        <li class="show active"><a data-toggle="tab" href="#punkt1">1</a></li>
         <li><a data-toggle="tab" href="#punkt2">2</a></li>
         <li><a data-toggle="tab" href="#punkt3">3</a></li>
         <li><a data-toggle="tab" href="#punkt4">4</a></li>
         <li><a data-toggle="tab" href="#punkt5">5</a></li>
         <li><a data-toggle="tab" href="#punkt6">6</a></li>
       </ul>
-
-    
-
-
+    </div>
   </div>
 </div>
 </div>
 
-    </div>
-
-</div>
+    
 
 
 
-</div>
 
-<!-- Hos dahlgaard-->
+<!-- Section nr 3 Hos dahlgaard-->
 <div class="container">
   <div class="row row-cols-2">
     <div class="col"><div class="p-2 bd-highlight">
-      <h2 class="  text-white font-weight-bold  pt-5">
-        Hos Dahlgaard </h2>
+      <h1 class="  text-white font-weight-bold  pt-5">
+        Hos Dahlgaard </h1>
     </div>
     <div class="p-2 bd-highlightw text-white">
       <p>En afgørende faktor for os er, at ejerkredsen samtidigt er en del af ledelsen, hvilket giver en hurtig og smidig beslutningsproces. For at opnå den nødvendige sparring udefra til ledelsen, har vi tilknyttet erfarne erhvervsledere i vor bestyrelse.</p>
-    </div> </div>
+    </div> 
+  </div>
+  
     <div class="col mt-7 p-7 ">
-      
-      <img src="/Images/omoselement.png" class="img-fluid" width="200" height="150" alt="Responsive image" /></div>
-    <div class="col pt-7 mt-7"><img src="/images/omoselement.png" class="img-fluid" width="400" height="300" alt="Responsive image" /></div>
+      <img src="/images/ansatte.jpg" class="img-fluid" width="400" height="400" alt="Responsive image" />
+    </div>
+
+    <div class="col pt-7 mt-7"><img src="/images/slide_1.jpg" class="img-fluid pb-7 mb-4" width="570" height="576" alt="Responsive image" />
+    </div>
     <div class="col">
       
-     
-
-    <div class="bd-highlight text-white">
+      <div class="bd-highlight text-white">
       <h6 class="font-weight-bold">
         Direktion, salg/kalkulation samt administration</h6> 
        </div> 
@@ -151,6 +142,14 @@
            </div> 
            <div class=" bd-highlightw font-weight-light text-white"><p>Da ejerkredsen samtidigt er en aktiv del af hverdagen i virksomheden og tillige med i bestyrelsen, har vi valgt at inddragne eksterne, erfarne erhvervsledere i vor bestyrelse, så der opnås en god sparring med input udefra.
            </p> </div>
+    
+
+      
+           
+           
+           
+           
+           
 
   </div>
   </div>
